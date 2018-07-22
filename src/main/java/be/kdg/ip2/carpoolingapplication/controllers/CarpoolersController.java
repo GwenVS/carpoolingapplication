9ff4a01abd39aee7fb178b079711c99514e4806c
gwendolenevansteenkiste <gwendolene.vansteenkiste@student.kdg.be>
2018-07-22 13:32:13 +0200

    adding crossorigin

--- src/main/java/be/kdg/ip2/carpoolingapplication/controllers/CarpoolersController.java
+++ src/main/java/be/kdg/ip2/carpoolingapplication/controllers/CarpoolersController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://carpoolingapplicationfe.herokuapp.com")
 @RequestMapping("/api/carpoolers")
 public class CarpoolersController {
 
